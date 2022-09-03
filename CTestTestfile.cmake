9ce86e7f5beecb2020c0c5a7ca8dd64ab5350013
Dave Carroll <12916551+dave-carroll7@users.noreply.github.com>
2022-09-03 15:52:04 -0700

    .gitignore + cleaning up structure

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/davecarroll/Projects/Audio/FFT/AudioFile
-# Build directory: /Users/davecarroll/Projects/Audio/FFT
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("examples")
-subdirs("tests")
