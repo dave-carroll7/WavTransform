1f60d8d66a878609d6f5a2853c606b540fac5ffd
Dave Carroll <12916551+dave-carroll7@users.noreply.github.com>
2022-09-03 20:17:06 -0700

    restructure and rename

---
+++ libs/AudioFile/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/davecarroll/Projects/Audio/WavTransform/libs/AudioFile
+# Build directory: /Users/davecarroll/Projects/Audio/WavTransform/libs/AudioFile
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("examples")
+subdirs("tests")
